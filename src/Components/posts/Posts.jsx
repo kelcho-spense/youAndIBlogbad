c650c334cdb74e42186fd9f4daa833009fc57af8
kelcho-spense <kelchospense88@gmail.com>
2022-09-14 13:00:22 +0300

    last deploy

--- src/Components/posts/Posts.jsx
+++ src/Components/posts/Posts.jsx
@@ -2,12 +2,12 @@ import Post from '../post/Post';
 import './posts.css';
 
 function Posts({ posts }) {
-  
+  console.log(posts);
   return (
     <div className='posts' >
-      {posts?.length > 0 && posts.map((p,index) => (
+      {posts?.length > 0 ?  posts.map((p,index) => (
         <Post post={p} key={index} />
-     ) )}
+     ) ): null}
      {console.log(posts)}
     </div>
   )
